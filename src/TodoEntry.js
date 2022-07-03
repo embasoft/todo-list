deb94f42cb7bc0e09615c3cff623117bff07fac6
Florian Beuscher <florianbeuscher@gmail.com>
2022-07-03 22:55:27 +0200

    Add animations for adding/removing tasks, feature to mark tasks as favorite, sorting tasks by their status, small design changes and eslint integration

--- src/TodoEntry.js
+++ src/TodoEntry.js
@@ -1,66 +1,102 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro';
-import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {solid, regular} from '@fortawesome/fontawesome-svg-core/import.macro';
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 
+/**
+ * Represents a todo task
+ * @param {Object} props Values of the todo task
+ * @return {React.ReactElement}
+ */
 function TodoEntry(props) {
-    // Initialize the todo state
-    const [todo, setTodo] = useState({
-        id: props.id,
-        text: props.text,
-        isFavorite: props.isFavorite,
-        isDone: props.isDone,
-        isDeleted: props.isDeleted,
-        saveHandler: props.saveHandler,
-        removeHandler: props.removeHandler
-    });
+  // Initialize the todo state
+  const [todo, setTodo] = useState({
+    id: props.id,
+    text: props.text,
+    isFavorite: props.isFavorite,
+    isDone: props.isDone,
+    isDeleted: props.isDeleted,
+    saveHandler: props.saveHandler,
+    removeHandler: props.removeHandler,
+    updateHandler: props.updateHandler,
+  });
 
-    // Request saving after a change of the todo occurs
-    React.useEffect(() => {
-        todo.saveHandler(todo);
-    }, [todo]);
+  // Request saving after a change of the todo occurs
+  React.useEffect(() => {
+    todo.saveHandler(todo);
+  }, [todo]);
 
-    {/* TODO Remove comments after properly implementing the sorting of favorite todos */}
-    // const toggleFavorite = (e) => {
-    //     e.preventDefault();
-    //     setTodo({...todo, isFavorite: !todo.isFavorite});
-    // }
+  /**
+   * Request to set the selected todo entry as favorite
+   * @param {Event} e Click event
+   */
+  const toggleFavorite = (e) => {
+    e.preventDefault();
+    setTodo({...todo, isFavorite: !todo.isFavorite});
+  };
 
-    /**
-     * Request to set the selected todo entry as done
-     * @param {Event} e Click event
-     */
-    const toggleDone = (e) => {
-        e.preventDefault();
-        setTodo({...todo, isDone: !todo.isDone});
-    }
+  /**
+   * Request to set the selected todo entry as done
+   * @param {Event} e Click event
+   */
+  const toggleDone = (e) => {
+    e.preventDefault();
+    setTodo({...todo, isDone: !todo.isDone});
+  };
 
-    /**
-     * Request to remove the selected todo entry
-     * @param {Event} e Click event
-     */
-    const deleteTask = (e) => {
-        e.preventDefault();
-        todo.removeHandler(todo);
-    }
-    
-    return (
-        <div>
-            <div className="flex justify-between py-2">
-                    <div className={`grow text-lg ${todo.isDone ? 'text-slate-400 line-through' : ''}`}>{todo.text}</div>
-                    {/* TODO Remove comments after properly implementing the sorting of favorite todos */}
-                    {/* <button type="button" onClick={toggleFavorite} className="px-1">
-                        <FontAwesomeIcon icon={todo.isFavorite ? solid('star') : regular('star')} className="text-yellow-400 hover:text-yellow-500" />
-                    </button> */}
-                    <div className="px-1" onClick={toggleDone}>
-                        <FontAwesomeIcon icon={todo.isDone ? solid('circle-check') : regular('circle-check')} className="text-green-600 hover:text-green-700 text-lg" />
-                    </div>
-                    <div className="px-1" onClick={deleteTask}>
-                        <FontAwesomeIcon icon={solid('trash-can')} className="text-slate-500 hover:text-red-500 text-lg" />
-                    </div>
-            </div>
-            <hr />
+  /**
+   * Request to remove the selected todo entry
+   * @param {Event} e Click event
+   */
+  const deleteTask = (e) => {
+    e.preventDefault();
+    todo.removeHandler(todo);
+  };
+
+  return (
+    <li>
+      <div className="flex justify-between py-2">
+        <div
+          className={`grow text-lg ${
+            todo.isDone ? 'text-slate-400 line-through' : ''
+          }`}
+        >
+          {todo.text}
         </div>
-    );
+        <button type="button" onClick={toggleFavorite} className="px-1">
+          <FontAwesomeIcon
+            icon={todo.isFavorite ? solid('star') : regular('star')}
+            className="text-lg text-yellow-400 hover:text-yellow-500"
+          />
+        </button>
+        <div className="px-1" onClick={toggleDone}>
+          <FontAwesomeIcon
+            icon={todo.isDone ? solid('circle-check') : regular('circle-check')}
+            className="text-lg text-green-600 hover:text-green-700"
+          />
+        </div>
+        <div className="px-1" onClick={deleteTask}>
+          <FontAwesomeIcon
+            icon={solid('trash-can')}
+            className="text-lg text-slate-500 hover:text-red-500"
+          />
+        </div>
+      </div>
+      <hr />
+    </li>
+  );
 }
+
+// Define the types of the properties
+TodoEntry.propTypes = {
+  id: PropTypes.number,
+  text: PropTypes.string,
+  isFavorite: PropTypes.bool,
+  isDone: PropTypes.bool,
+  isDeleted: PropTypes.bool,
+  saveHandler: PropTypes.func,
+  removeHandler: PropTypes.func,
+  updateHandler: PropTypes.func,
+};
 
 export default TodoEntry;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,107 +1,174 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
-import React, { useRef, useState } from "react";
-import TodoEntry from "./TodoEntry";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {solid} from '@fortawesome/fontawesome-svg-core/import.macro';
+import React, {useRef, useState} from 'react';
+import TodoEntry from './TodoEntry';
+import {useTransition, animated} from 'react-spring';
 
+/**
+ * List of todo entries and a form to add new ones
+ * @return {React.Element}
+ */
 function TodoList() {
-    // Initialize the todos state by using existing ones from the local storage, if there are any saved ones
-    const [todos, setTodos] = useState(() => {
-        const savedData = window.localStorage.getItem('todos');
-        return savedData != null ? JSON.parse(savedData) : [];
+  /**
+   * Initialize the todos state by using existing ones from the local storage,
+   * if there are any saved ones
+   */
+  const [todos, setTodos] = useState(() => {
+    const savedData = window.localStorage.getItem('todos');
+    return savedData != null ? JSON.parse(savedData) : [];
+  });
+  const todosRef = useRef();
+  todosRef.current = todos;
+
+  React.useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  /**
+   * Save all todos to the local storage after receiving a request to save
+   */
+  const saveTodos = () => {
+    window.localStorage.setItem('todos', JSON.stringify(todosRef.current));
+  };
+
+  /**
+   * Update the specified todo after receiving a request to save
+   * @param {TodoEntry} changedTodo
+   */
+  const updateTodo = (changedTodo) => {
+    const newTodos = todosRef.current;
+    console.log('updated');
+
+    // Update the changed todo entry
+    const todoId = newTodos.findIndex((todo) => {
+      return todo.id === changedTodo.id;
     });
-    const todosRef = useRef();
-    todosRef.current = todos;
+    newTodos[todoId] = changedTodo;
+    setTodos(newTodos);
+    saveTodos();
+  };
 
-    React.useEffect(() => {
-        window.localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
+  /**
+   * Add a new todo entry based on the text input field
+   * @param {Event} e Click event
+   * @return {Boolean}
+   */
+  const addTodo = (e) => {
+    e.preventDefault();
 
-    /**
-     * Save all todos to the local storage after receiving a request to save
-     */
-    const saveTodos = () => {
-        window.localStorage.setItem('todos', JSON.stringify(todosRef.current));
-    }
+    setTodos([
+      ...todos,
+      {
+        id: generateId(),
+        text: e.target.firstChild.value,
+        isFavorite: false,
+        isDone: false,
+        isDeleted: false,
+        saveHandler: saveTodos,
+        removeHandler: removeTodo,
+      },
+    ]);
 
-    /**
-     * Update the specified todo after receiving a request to save
-     * @param {TodoEntry} changedTodo 
-     */
-     const updateTodo = (changedTodo) => {
-        const newTodos = todosRef.current;
+    // Reset the text input field
+    e.target.firstChild.value = '';
 
-        // Update the changed todo entry
-        const todoId = newTodos.findIndex((todo) => {
-            return todo.id === changedTodo.id;
-        });
-        newTodos[todoId] = changedTodo;
-        setTodos(newTodos);
-        saveTodos();
-    }
+    return false;
+  };
 
-    /**
-     * Add a new todo entry based on the text input field
-     * @param {Event} e Click event
-     * @returns 
-     */
-    const addTodo = (e) => {
-        e.preventDefault();
+  /**
+   * Remove the given todo
+   * @param {TodoEntry} obsoleteTodo
+   */
+  const removeTodo = (obsoleteTodo) => {
+    // Return every todo except the one which should be filtered out
+    const newTodos = todosRef.current.filter((todo) => {
+      return todo.id !== obsoleteTodo.id;
+    });
+    setTodos(newTodos);
+  };
 
-        setTodos([...todos, {
-            id: generateId(),
-            text: e.target.firstChild.value,
-            isFavorite: false,
-            isDone: false,
-            isDeleted: false,
-            saveHandler: saveTodos,
-            removeHandler: removeTodo
-        }]);
-        
-        // Reset the text input field
-        e.target.firstChild.value = '';
+  /**
+   * Generate a new, unique id based on the current ones
+   * @return {Number} New, unique id
+   */
+  const generateId = () => {
+    const maxId = Math.max(...todosRef.current.map((todo) => todo.id));
+    return isFinite(maxId) ? maxId + 1 : 0;
+  };
 
-        return false;
-    }
+  /**
+   * Sort the todos in such a way that the favorized tasks appear first,
+   * then the normal one and finally the done tasks
+   * @param {Array} todos List of TodoEntry objects
+   * @return {Array} Sorted todos
+   */
+  const sortByPriority = (todos) => {
+    const favorites = [];
+    const normals = [];
+    const dones = [];
 
-    /**
-     * Remove the given todo
-     * @param {TodoEntry} obsoleteTodo 
-     */
-    const removeTodo = (obsoleteTodo) => {
-        // Return every todo except the one which should be filtered out
-        const newTodos = todosRef.current.filter((todo) => {
-            return todo.id !== obsoleteTodo.id;
-        });
-        setTodos(newTodos);
-    }
+    todos.forEach((todo) => {
+      if (todo.isDone) dones.push(todo);
+      else if (todo.isFavorite) favorites.push(todo);
+      else normals.push(todo);
+    });
 
-    /**
-     * Generate a new, unique id based on the current ones
-     * @returns {Number} New, unique id
-     */
-    const generateId = () => {
-        const maxId = Math.max(...todosRef.current.map((todo) => todo.id));
-        return isFinite(maxId) ? maxId + 1 : 0;
-    }
-    
-    return (
-        <div className="TodoList max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl mx-auto border border-slate-300 p-2 m-2 rounded-md shadow-md">
-            <h1 className="text-2xl font-bold">To-Do List</h1>
-            <form className="flex justify-between py-1 pb-3 text-lg" onSubmit={addTodo}>
-                <input type="text" placeholder="Aufgabe hinzufügen" className="grow outline-blue-500 rounded-md p-1" />
-                <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-md p-1 px-2 ml-2">
-                    <FontAwesomeIcon icon={solid('file-circle-plus')} /> <span className="hidden sm:inline-block">Hinzufügen</span>
-                </button>
-            </form>
-            <hr />
-            <div className="">
-                {/* Draw every todo entry */}
-                {todos.map((todo) => {
-                    return <TodoEntry id={todo.id} key={todo.id} text={todo.text} isFavorite={todo.isFavorite} isDone={todo.isDone} isDeleted={todo.isDeleted} saveHandler={updateTodo} removeHandler={removeTodo}></TodoEntry>;
-                })}
-            </div>
-        </div>
-    );
+    return favorites.concat(normals, dones);
+  };
+
+  // Define the style of the adding/removing animations
+  const transitions = useTransition(sortByPriority(todos), {
+    config: {
+      duration: 250,
+    },
+    expires: 0,
+    from: {opacity: 0},
+    enter: {opacity: 1},
+    leave: {opacity: 0},
+    keys: todos.map((todo, index) => todo.id),
+  });
+
+  return (
+    <div className="TodoList max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl
+                    mx-auto border border-slate-300 p-2 m-2 rounded-md
+                    shadow-md">
+      <h1 className="text-2xl font-bold">To-Do List</h1>
+      <form
+        className="flex justify-between py-1 pb-3 text-lg"
+        onSubmit={addTodo}
+      >
+        <input
+          type="text"
+          placeholder="Aufgabe hinzufügen"
+          className="grow outline-blue-500 rounded-md p-1"
+        />
+        <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-md
+                           p-1 px-2 ml-2">
+          <FontAwesomeIcon icon={solid('file-circle-plus')} />{' '}
+          <span className="hidden sm:inline-block">Hinzufügen</span>
+        </button>
+      </form>
+      <hr />
+      {/* Draw every todo entry */}
+      <ul>
+        {transitions((styles, todo) => (
+          <animated.div style={styles}>
+            <TodoEntry
+              id={todo.id}
+              key={todo.id}
+              text={todo.text}
+              isFavorite={todo.isFavorite}
+              isDone={todo.isDone}
+              isDeleted={todo.isDeleted}
+              saveHandler={updateTodo}
+              removeHandler={removeTodo}
+              updateHandler={updateTodo}
+            ></TodoEntry>
+          </animated.div>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
